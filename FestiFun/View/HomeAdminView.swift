af17d22c220608f90f54716e68777d5b4f91efb2
Laulilaura <laulilaura@orange.fr>
2023-03-27 17:06:54 +0200

    derniers changement

--- FestiFun/View/HomeAdminView.swift
+++ FestiFun/View/HomeAdminView.swift
@@ -16,37 +16,7 @@ struct HomeAdminView: View {
     
     var body: some View {
         VStack {
-            if !errorMessage.isEmpty {
-                Text(errorMessage).foregroundColor(.red)
-            } else {
-                if(benevoles.isEmpty){
-                    Text("Il n'existe pas encore de bénévole").italic()
-                } else {
-                    ForEach(benevoles, id: \.id) { benevole in
-                        VStack(alignment: .leading) {
-                            Text(benevole.nom).bold()
-                            Text(benevole.email).italic()
-                        }.padding()
-                        .cornerRadius(5.0)
-                        .background(
-                            RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.lightyellow, lineWidth: 1)
-                            .background(Color.lightyellow)
-                            .frame(width: 280, height: 60)
-                        )
-                    }
-                }
-            }
-        }
-        .onAppear {
-            Task {
-                switch await BenevoleDAO.shared.getAllBenevole() {
-                case .failure(let error):
-                    errorMessage = "Erreur : \(error.localizedDescription)"
-                case .success(let benevoles):
-                    self.benevoles = benevoles
-                }
-            }
+            Text("Admin page home")
         }
     }
 }
