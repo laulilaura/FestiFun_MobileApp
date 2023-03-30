12e9a0585b83d525036da6cb5ca5d2b11966c06d
Laulilaura <laulilaura@orange.fr>
2023-03-30 14:31:21 +0200

    Récupération code corentin + view à jour

--- FestiFun/View/BenevolesListAdminView.swift
+++ FestiFun/View/BenevolesListAdminView.swift
@@ -9,52 +9,15 @@ import SwiftUI
 
 struct BenevolesListAdminView: View {
     
-    @StateObject var loggedBenevole: LoggedBenevole
+    @EnvironmentObject var loggedBenevole: LoggedBenevole
     
     @State var errorMessage = ""
     @State var benevoles: [Benevole] = []
     
     var body: some View {
-        TabView{
-            NavigationView{
-                HomeAdminView(loggedBenevole: loggedBenevole)
-            }
-            .tabItem{
-                Image(systemName: "house")
-                Text("Home")
-            }
-            .tint(Color("salmon"))
-            
-            NavigationView{
-                FestivalListBenevoleView(loggedBenevole: loggedBenevole)
-                    .navigationTitle("Bénévoles")
-            }
-            .tabItem{
-                Image(systemName: "person.3.fill")
-                Text("Bénévoles")
-            }
-            
-            
-            NavigationView{
-                FestivalListBenevoleView(loggedBenevole: loggedBenevole)
-                    .navigationTitle("Gestion des festivals")
-            }
-            .tabItem{
-                Image(systemName: "signpost.right.and.left.fill")
-                Text("Festivals")
-            }
-            
-            
-            NavigationView{
-                HomeAdminView(loggedBenevole: loggedBenevole)
-                    .navigationTitle("Paramètres")
-            }
-            .tabItem{
-                Image(systemName: "person.circle")
-                Text("Paramètres")
-            }
+        VStack {
+            Text("Liste de bénévole")
         }
-        .accentColor(.salmon)
     }
 }
 
---
+++ FestiFun/View/FestivalFormView.swift
@@ -0,0 +1,20 @@
+//
+//  FestivalFormView.swift
+//  FestiFun
+//
+//  Created by etud on 30/03/2023.
+//
+
+import SwiftUI
+
+struct FestivalFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FestivalFormView_Previews: PreviewProvider {
+    static var previews: some View {
+        FestivalFormView()
+    }
+}
